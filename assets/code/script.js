616e4bf11f353f7d69365e23efac1ffea2e9082f
Jordan <jnbarringer96@gmail.com>
2022-10-20 14:48:21 -0500

    added title, fixed weather URL variable, made it so the function doesn't run before it's called

--- assets/code/script.js
+++ assets/code/script.js
@@ -1,5 +1,5 @@
 //create initial variables from HTML
-var input = document.querySelector("#user-input");
+let input = document.querySelector("#user-input").value;
 var searchBtn = document.querySelector("#search");
 var searchHist = document.querySelector("#search-history");
 var cityCurrent = document.querySelector("#city-current");
@@ -7,12 +7,16 @@ var forecast = document.querySelector("#five-day-forecast");
 
 //create variables from weather server API
 var APIKey = "f299a5114970c339b29accfb86e8b629"
-var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=" + input.value + APIKey
+var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=" + input.value + "&appid=" + APIKey
+
+//create function that goes in the search button eventlistener
+function searchFunc (event) {
+    event.preventDefault()
+    input = input.value
+    console.log(input);
+    //input goes to search history
+    //current and future weather conditions are presented 
+}
 
 //make search button search
 searchBtn.addEventListener("click", searchFunc)
-
-//create function that goes in the searchbutton eventlistener
-function searchFunc () {
-    console.log(input.innerText);
-}
