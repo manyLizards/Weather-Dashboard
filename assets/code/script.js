07dc687e6464f11518f3595ddf2873ebe6ec5cba
Jordan <jnbarringer96@gmail.com>
2022-10-20 23:32:45 -0500

    moved the weatherURL var so that the input within it has the user input rather than being undefined

--- assets/code/script.js
+++ assets/code/script.js
@@ -1,5 +1,5 @@
 //create initial variables from HTML
-let input = document.querySelector("#user-input").value;
+let input = document.querySelector("#user-input");
 var searchBtn = document.querySelector("#search");
 var searchHist = document.querySelector("#search-history");
 var cityCurrent = document.querySelector("#city-current");
@@ -7,16 +7,22 @@ var forecast = document.querySelector("#five-day-forecast");
 
 //create variables from weather server API
 var APIKey = "f299a5114970c339b29accfb86e8b629"
-var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=" + input.value + "&appid=" + APIKey
 
 //create function that goes in the search button eventlistener
 function searchFunc (event) {
     event.preventDefault()
     input = input.value
     console.log(input);
+    var weatherURL = "api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIKey
     //input goes to search history
     //current and future weather conditions are presented 
+    $.ajax({
+        url: weatherURL,
+        method: 'GET',
+    }).then(function (response) {
+        console.log('AjaxResponse: ' + response);
+    })
 }
 
 //make search button search
-searchBtn.addEventListener("click", searchFunc)
+searchBtn.addEventListener("click", searchFunc);
