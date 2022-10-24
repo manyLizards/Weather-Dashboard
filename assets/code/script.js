43d8867b86169b3bef018e97d167f965c55698ba
Jordan <jnbarringer96@gmail.com>
2022-10-24 12:28:46 -0500

    commented out divs/started a for loop for future weather

--- assets/code/script.js
+++ assets/code/script.js
@@ -19,6 +19,19 @@ var fifth = document.querySelector("#fifth")
 //create variable for API key
 var APIKey = "f299a5114970c339b29accfb86e8b629"
 
+//Create a function for future weather
+function futureWeather (data) {
+    console.log(data.list)
+    
+    //create for loop that picks 5 indexes off the array at 24 hr intervals
+    for (var i = 2; i < data.list.length; i += 8) {
+        console.log(data.list[i])
+        var headingEl = document.createElement("h3")
+        headingEl.innerText = "didgeridoo"
+        forecast.appendChild(headingEl)
+    }
+}
+
 //create function that goes in the search button eventlistener
 function searchFunc (event) {
     event.preventDefault()
@@ -53,43 +66,47 @@ function searchFunc (event) {
                 //add temperature
                 var temp = document.createElement("li")
                 currentParameters.appendChild(temp)
-                temp.innerHTML = data.list[0].main.temp
+                temp.innerHTML = "Temperature in Kelvin: " + data.list[0].main.temp
 
                 //add humidity
                 var humidity = document.createElement("li")
                 currentParameters.appendChild(humidity)
-                humidity.innerHTML = data.list[0].main.humidity
+                humidity.innerHTML = "Humidity: " + data.list[0].main.humidity + "%"
 
                 //add wind speed
                 var windSpeed = document.createElement("li")
                 currentParameters.appendChild(windSpeed)
-                windSpeed.innerHTML = data.list[0].wind.speed
+                windSpeed.innerHTML = "Wind speed: " + data.list[0].wind.speed + "mph"
 
-                //First future weather conditions
+                // //First future weather conditions
 
-                //date
-                var dateFirst = 
-                document.createElement("h4");
-                first.appendChild(dateFirst);
-                dateFirst.innerHTML = data.list[1].dt_txt
+                // //date
+                // var dateFirst = 
+                // document.createElement("h4");
+                // console.log(first)
+                // first.appendChild(dateFirst);
+                // dateFirst.innerHTML = data.list[1].dt_txt
 
-                //add icon representation of weather conditions
-                var iconFirst = document.createElement("img");
+                // //add icon representation of weather conditions
+                // var iconFirst = document.createElement("img");
 
-                //add temperature
-                var tempFirst = document.createElement("li")
-                firstParameters.appendChild(tempFirst)
-                tempFirst.innerHTML = data.list[1].main.temp
+                // //add temperature
+                // var tempFirst = document.createElement("li")
+                // firstParameters.appendChild(tempFirst)
+                // tempFirst.innerHTML = data.list[1].main.temp
 
-                //add humidity
-                var humidityFirst = document.createElement("li")
-                firstParameters.appendChild(humidity)
-                humidityFirst.innerHTML = data.list[1].main.humidity
+                // //add humidity
+                // var humidityFirst = document.createElement("li")
+                // firstParameters.appendChild(humidity)
+                // humidityFirst.innerHTML = data.list[1].main.humidity
 
-                //add wind speed
-                var windSpeedFirst = document.createElement("li")
-                firstParameters.appendChild(windSpeedFirst)
-                windSpeedFirst.innerHTML = data.list[1].wind.speed
+                // //add wind speed
+                // var windSpeedFirst = document.createElement("li")
+                // firstParameters.appendChild(windSpeedFirst)
+                // windSpeedFirst.innerHTML = data.list[1].wind.speed
+
+                //call a function to display future weather conditions
+                futureWeather(data)
             })
         }
     }
